700bdbf56e8875d58c31d16eddab4f8ae5043a5c
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-04-29 09:48:49 -0400

    testing to see if max > 0 in cRange.Next()
    cProblem.

--- AddingGame/cRange.cs
+++ AddingGame/cRange.cs
@@ -19,7 +19,7 @@ namespace AddingGame
         }
 
         /// <summary>
-        /// Call Random.Next(_min, _max+1)
+        /// Return a random value betwen _min and _max, inclusive.
         /// </summary>
         /// <returns>
         /// Returns a random value between and including the values
@@ -27,7 +27,21 @@ namespace AddingGame
         /// </returns>
         public int Next()
         {
-            return _rand.Next(_min, _max + 1);
+            if (_max == 0)
+                return 0;
+            else
+                return _rand.Next(_min, _max + 1);
+        }
+
+        /// <summary>
+        /// Set a new Range for this instance of cRange.
+        /// </summary>
+        /// <param name="min">new minimum value</param>
+        /// <param name="max">new maximum value</param>
+        public void SetRange(int min, int max)
+        {
+            _min = min;
+            _max = max;
         }
 
 
